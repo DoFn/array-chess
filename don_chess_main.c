d6662ef21aa650d4fdc297e02f7e079f816d1655
DoFn <thedonenzo@gmail.com>
2024-12-21 22:55:10 +1100

    Update don_chess_main.c

--- don_chess_main.c
+++ don_chess_main.c
@@ -9,6 +9,11 @@
 // This program allows the user to play a simple game of chess with another person via
 // the use of a 2D array struct coupled with user commands in the terminal. This program
 // utilises three files (don_chess_main.c, don_chess_functions.c and don_chess_header.h).
+//
+// Limitations
+// This program will only trigger stalemate when one player cannot make a move, it
+// will not trigger due to other conditions such as too many king moves or
+// move repetition.
 
 // Header File
 #include "don_chess_header.h"
